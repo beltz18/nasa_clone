1151d74b20f2a2cbd664aa147ba1ab0cf76f48f3
beltz18 <beltz.18kyodai@gmail.com>
2022-09-07 17:09:38 -0400

    changes to see whatś happening on build

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,6 +7,7 @@ const { title, arrUl1, arrUl2, arrUl3, infoS } = require('../../util/global')
 router.get(process.env.ROOT, async (req,res) => {
   const arrFiles  = await require('../../util/files')
   res.render('index', { img: arrFiles, title, arrUl1, arrUl2, arrUl3, infoS })
+  console.log(arrFiles)
 })
 
 module.exports = router
