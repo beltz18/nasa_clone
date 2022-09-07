5c80947066f1df8d3b1fb194bc57f4e97c866619
beltz18 <beltz.18kyodai@gmail.com>
2022-09-07 16:24:53 -0400

    first final version

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 
 const { Router } = require('express')
 const router     = Router()
-const { title, arrUl1, arrUl2 } = require('../../util/global')
+const { title, arrUl1, arrUl2, arrUl3, infoS } = require('../../util/global')
 
 router.get(process.env.ROOT, async (req,res) => {
   const arrFiles  = await require('../../util/files')
-  res.render('index', { img: arrFiles, title, arrUl1, arrUl2 })
+  res.render('index', { img: arrFiles, title, arrUl1, arrUl2, arrUl3, infoS })
 })
 
 module.exports = router
--- util/global.js
+++ util/global.js
@@ -1,5 +1,15 @@
-const title    = "NASA"
-const arrUl1   = ['Missions', 'Galleries', 'NASA TV', 'Follow NASA', 'Downloads', 'About', 'NASA Audiences']
-const arrUl2   = ['Humans in Space', 'Moon to Mars', 'Earth', 'Space Tech', 'Flight', 'Solar System and Beyond', 'STEM Engagement', 'History', 'Benefits to You']
+const title  = "NASA"
+const arrUl1 = ['Missions', 'Galleries', 'NASA TV', 'Follow NASA', 'Downloads', 'About', 'NASA Audiences']
+const arrUl2 = ['Humans in Space', 'Moon to Mars', 'Earth', 'Space Tech', 'Flight', 'Solar System and Beyond', 'STEM Engagement', 'History', 'Benefits to You']
+const arrUl3 = ['No Fear Act', 'FOIA', 'Privacy', 'Office of Inspector General', 'Office of Special Counsel', 'Agency Financial Reports', 'Contact NASA']
+const infoS  = {
+  fs: ['Webb Telescope', 'Solar System and Beyond', 'Artemis I'],
+  ss: [
+        "Artemis Teams Continue to Review Launch Options, Prepare to Replace Seal",
+        "NASA's Webb Takes Its First-Ever Direct Image of Distant World",
+        "Explore the Sola System With NASA's New-and-Improved 3D 'Eyes'",
+        "Artemis I Travel Guide: The Essentials for Your Trip to the Moon"
+      ]
+}
 
-module.exports = { title, arrUl1, arrUl2 }
+module.exports = { title, arrUl1, arrUl2, arrUl3, infoS }
